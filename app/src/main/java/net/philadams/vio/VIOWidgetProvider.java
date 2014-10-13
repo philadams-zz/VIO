d85b0b0271684abc6e3aceaf4a9f8d80bbce38ec
Phil Adams <philadams.net@gmail.com>
2014-10-13 13:46:58 -0400

    initial commit: stub vio widget

---
+++ app/src/main/java/net/philadams/vio/VIOWidgetProvider.java
@@ -0,0 +1,12 @@
+package net.philadams.vio;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by phil on 10/13/14.
+ */
+public class VIOWidgetProvider extends AppWidgetProvider {
+
+
+
+}
