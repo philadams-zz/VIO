9f8aa9a9b688bef4d1abc25fbbd61e9a1fedf8cc
Phil Adams <philadams.net@gmail.com>
2014-10-20 21:24:06 -0400

    add dark outline to vio widget

--- app/src/main/java/net/philadams/vio/VIOWidgetProvider.java
+++ app/src/main/java/net/philadams/vio/VIOWidgetProvider.java
@@ -16,6 +16,7 @@ import java.util.Random;
 public class VIOWidgetProvider extends AppWidgetProvider {
 
   final String TAG = "VIOWidgetProvider";
+  private int outlineShape = R.drawable.vio_outline_shape;
 
   @Override
   public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -25,10 +26,15 @@ public class VIOWidgetProvider extends AppWidgetProvider {
     for (int widgetId : allWidgetIds) {
 
       int alpha = getAlpha();
+      outlineShape =
+          (outlineShape == R.drawable.vio_outline_shape) ? R.drawable.vio_outline_shape_light
+              : R.drawable.vio_outline_shape;
       Log.d(TAG, String.format("alpha: %d", alpha));
 
-      RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.vio_widget_layout);
-      remoteViews.setInt(R.id.vio_image_view, "setImageAlpha", alpha);
+      RemoteViews remoteViews =
+          new RemoteViews(context.getPackageName(), R.layout.vio_widget_layout);
+      remoteViews.setInt(R.id.vio_widget_fill, "setImageAlpha", alpha);
+      remoteViews.setInt(R.id.vio_widget_outline, "setImageResource", outlineShape);
       //remoteViews.setTextViewText(R.id.update, String.valueOf(number));
 
       // pending intent for widget onclick that posts a 'thinking' via the VIOAPIService
@@ -40,11 +46,9 @@ public class VIOWidgetProvider extends AppWidgetProvider {
       // update widget
       appWidgetManager.updateAppWidget(widgetId, remoteViews);
     }
-
   }
 
   private int getAlpha() {
     return (new Random().nextInt(255));  // this value will really be set from the API
   }
-
 }
